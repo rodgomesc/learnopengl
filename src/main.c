603b23a04ad4dd89de46b68396b300db6a9d52ee
rodgomesc <rodgomesc@gmail.com>
2025-04-07 16:43:28 -0300

    Implement window resizing and input handling in main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+} 
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main() {
     printf("GLFW version %d.%d\n", GLFW_VERSION_MAJOR, GLFW_VERSION_MINOR);
@@ -21,22 +34,25 @@ int main() {
         printf("Failed to create GLFW window\n");
         glfwTerminate();
         return -1;
-    }
-
-    
+    }    
     glfwMakeContextCurrent(window);
-
    
-    
     if (!gladLoadGL(glfwGetProcAddress)) {
         printf("Failed to initialize GLAD\n");
         return -1;
     }
- 
+
+    // callback for window resizing
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
     /*   ----------------- Game Loop ----------------- */
     while (!glfwWindowShouldClose(window))
     {
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
